28fa6e8d6c89c40536672d776b55e8a570568300
Rohith <kaveri.rohit@gmail.com>
2022-11-21 15:09:07 -0500

    Initial Commit

---
+++ Shared/GameCalciApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameCalciApp.swift
+//  Shared
+//
+//  Created by Rohith K on 11/21/22.
+//
+
+import SwiftUI
+
+@main
+struct GameCalciApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
